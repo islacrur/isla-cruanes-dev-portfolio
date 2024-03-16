47adb2fb27b8792390c6477b301773ac813f7d2f
islacrur <islacrur@gmail.com>
2024-03-16 21:34:57 +0100

    Create Button component

---
+++ frontend/src/components/atoms/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <>
+    <button type="button" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Button</button>
+
+    </>
+  )
+}
+
+export default Button
