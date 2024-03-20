8c1b420785e8c814a5a7c102a0bd98e069c62493
islacrur <islacrur@gmail.com>
2024-03-20 13:00:41 +0100

    Correct ProjectSeeder

--- backend/app/Models/Project.php
+++ backend/app/Models/Project.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'name', 
         'short_description', 
